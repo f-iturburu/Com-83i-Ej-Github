0fd0ee3359b47aacc2551e867d923c12622cb5cf
Lucia Craven Garat <luciacraven93@gmail.com>
2023-09-03 18:25:52 -0300

    mensaje Lucia

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 document.open()
 document.write(`<h1>Fran was here</h1>`)
-//Deja tu mensaje aca
+//Hola Fran
 document.write(`<p>Cristian is here</p>`)
 //otro mensaje
 document.write(`<h2>Santi was here</h2>`)
