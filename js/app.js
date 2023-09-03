2e21330d906099dafcfcb7c59c2deca419ace88c
cristianlzelaya <cristianlzelaya@gmail.com>
2023-09-03 13:03:36 -0300

    ingrese su celular

--- js/app.js
+++ js/app.js
@@ -2,4 +2,6 @@ document.open()
 document.write(`<h1>Fran was here</h1>`)
 //Deja tu mensaje aca
 document.write(`<p>Cristian is here</p>`)
+let celular = parseInt(prompt(`Ingrese su celular`),10);
+console.log(celular)
 document.close()
