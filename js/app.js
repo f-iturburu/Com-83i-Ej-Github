bc4c1489187fbbf4df788e144a319ce7b0e3a674
SantiCampero <thesantiblocks@gmail.com>
2023-09-03 15:38:34 -0300

    nuevo mensaje

--- js/app.js
+++ js/app.js
@@ -2,4 +2,5 @@ document.open()
 document.write(`<h1>Fran was here</h1>`)
 //Deja tu mensaje aca
 document.write(`<p>Cristian is here</p>`)
+document.write(`<h2>Santi was here</h2>`)
 document.close()
