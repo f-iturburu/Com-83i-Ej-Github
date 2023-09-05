0efff3467604605b1976a3f6d3b8d03ca8324997
Matías León <matiasleon.br.2010@gmail.com>
2023-09-05 19:28:46 -0300

    Commit Leon

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-document.open()
-document.write(`<h1>Fran was here</h1>`)
+document.open();
+document.write(`<h1>Fran was here</h1>`);
 //Deja tu mensaje aca
-document.write(`<h3>hola mundo</h3>`)
-document.write(`<p>Cristian is here</p>`)
+document.write(`<h3>hola mundo</h3>`);
+document.write(`<p>Cristian is here</p>`);
+document.write(`<h2>¡Acá León!</h2>`);
+
 //otro mensaje
 
-document.write(`<h2>Santi was here</h2>`)
+document.write(`<h2>Santi was here</h2>`);
 //Bruno Mensaje
-document.write(`<h2>Bruno was here!</h2>`)
-document.close()
+document.write(`<h2>Bruno was here!</h2>`);
+document.close();
